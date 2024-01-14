77683b071b9cdaa65b5623ecbc2543e742002461
Bethalynn <bethalynnyeo@gmail.com>
2024-01-14 14:34:16 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+#To do list
+
+from datetime import datetime
+
+database = {
+    datetime.now():
+    {
+    'todoItem': 'template',
+    'current': datetime.now(),
+    'IsCompleted': False,
+    'Category': 'template2'
+    }
+}
+
+user_input = input("Enter\t")
+while user_input != "Q":
+    user_input = input("Enter a command\t")
+    if user_input == "Create":
+       todoItem = input("Enter a new item todo:\t")
+       currentTime = datetime.now()
+       IsCompleted = False
+       Category = input("Enter category:\t") 
+
+       print(f"""
+        Todo_item: {todoItem}
+        CreateDateTime: {currentTime}
+        IsCompleted: {IsCompleted}
+        Category: {Category}
+       """)
+       database [datetime.now()] = {
+           'todoItem': todoItem,
+           'CreateDateTime': currentTime,
+           'IsCompleted': IsCompleted,
+           'Category': Category
+       }
+
+    elif user_input == "Display" :
+        for i in database:
+            print(database[i])    
